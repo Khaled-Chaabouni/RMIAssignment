1f95c05fa559538e5e16f37a4d3bc50af7258773
Khaled-Chaabouni <63324267+Khaled-Chaabouni@users.noreply.github.com>
2021-12-10 23:58:54 +0100

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -2,5 +2,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyInterface extends Remote {
-
+    public String reverse(String s) throws RemoteException;
+    public char minchar(String s) throws RemoteException;
+    public String casechanger(String s) throws RemoteException;
 }
