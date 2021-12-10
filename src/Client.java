d17c413f33d17fb0fa95f69389a10b0bf46cac06
Khaled-Chaabouni <63324267+Khaled-Chaabouni@users.noreply.github.com>
2021-12-10 23:58:24 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,30 @@
 import java.rmi.Naming;
+import java.util.Scanner;
+
 
 public class Client {
 
     public static void main(String[] args) {
         try {
-           MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            Scanner scanner=new Scanner(System.in);
 
+            //Using the reverse Service :
+            System.out.println("Input for reverse :");
+            String str1 = scanner.nextLine();
+            System.out.println(service.reverse(str1));
+
+            //Using the minchar Service :
+            System.out.println("Input for minchar :");
+            String str2 = scanner.nextLine();
+            System.out.println(service.minchar(str2));
+
+            //Using the casechanger Service :
+            System.out.println("Input for casechanger :");
+            String str3 = scanner.nextLine();
+            System.out.println(service.casechanger(str3));
+
+            scanner.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
